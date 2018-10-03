7a6c0d4d282db9b5b0809fb9c3fcacfe69c08229
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-10-03 13:43:27 +0200

    Creates tables for fruktkorg-service

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE fruktkorg(
+  fruktkorg_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  last_changed TIMESTAMP DEFAULT now() NOT NULL
+);
+
+CREATE TABLE frukt(
+  frukt_id SERIAL PRIMARY KEY,
+  type TEXT NOT NULL,
+  amount INTEGER NOT NULL,
+  fruktkorg_id INTEGER REFERENCES fruktkorg(fruktkorg_id) ON DELETE CASCADE
+);
