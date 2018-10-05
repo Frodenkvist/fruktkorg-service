30d40bf977cac3923371c4b38e1da3231453a436
Alexander Sundström <alexander.sundstrom@gmail.com>
2018-10-05 15:45:03 +0200

    Adds Controller tests for Fruktkorgar

--- src/main/test/java/com/fruktkorgservice/FruktkorgControllerTest.java
+++ src/main/test/java/com/fruktkorgservice/FruktkorgControllerTest.java
@@ -2,10 +2,14 @@ package com.fruktkorgservice;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fruktkorgservice.common.exception.FruktkorgMissingException;
 import com.fruktkorgservice.common.model.Frukt;
 import com.fruktkorgservice.common.model.Fruktkorg;
+import com.fruktkorgservice.common.model.dto.FruktCreateDTO;
+import com.fruktkorgservice.common.model.dto.FruktUpdateDTO;
+import com.fruktkorgservice.common.model.dto.FruktkorgCreateDTO;
+import com.fruktkorgservice.common.model.dto.FruktkorgUpdateDTO;
 import com.fruktkorgservice.controller.FruktkorgController;
 import com.fruktkorgservice.service.FruktkorgService;
 import org.junit.Assert;
@@ -19,15 +23,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.util.CollectionUtils;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -65,6 +71,7 @@ public class FruktkorgControllerTest {
                 new TypeReference<List<Fruktkorg>>() {
                 });
         Assert.assertNotNull(returnFruktkorgar);
+        Assert.assertEquals(fruktkorgar, returnFruktkorgar);
 
     }
 
@@ -85,9 +92,153 @@ public class FruktkorgControllerTest {
 
     }
 
+    @Test
+    public void getMissingFruktkorg() throws Exception {
+        when(fruktkorgService.getFruktkorg(1l)).thenThrow(FruktkorgMissingException.class);
+        mvc.perform(get("/v1/fruktkorg/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(404));
+    }
+
+    @Test
+    public void findByFrukt() throws Exception {
+        List<Fruktkorg> fruktkorgar = createExampleFruktkorgar();
+        when(fruktkorgService.getFruktkorgarByFruktType(PEAR_NAME)).thenReturn(fruktkorgar);
+
+        MvcResult result = mvc.perform(get("/v1/fruktkorg/frukt/" + PEAR_NAME)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<Fruktkorg> returnFruktkorgar = objectMapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<List<Fruktkorg>>() {
+                });
+        Assert.assertNotNull(returnFruktkorgar);
+        Assert.assertEquals(fruktkorgar, returnFruktkorgar);
+
+    }
+
+    @Test
+    public void noFruktFound() throws Exception {
+        when(fruktkorgService.getFruktkorgarByFruktType(PEAR_NAME)).thenReturn(new ArrayList<>());
+
+        MvcResult result = mvc.perform(get("/v1/fruktkorg/frukt/" + PEAR_NAME)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<Fruktkorg> returnFruktkorgar = objectMapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<List<Fruktkorg>>() {
+                });
+        Assert.assertNotNull(returnFruktkorgar);
+        Assert.assertEquals(new ArrayList<>(), returnFruktkorgar);
+
+    }
+
+    @Test
+    public void createFruktkorg() throws Exception {
+        FruktkorgCreateDTO fruktkorgDTO = new FruktkorgCreateDTO();
+        fruktkorgDTO.setName(FRUKTKORG_NAME);
+        fruktkorgDTO.setLastChanged(NOW);
+
+        FruktCreateDTO fruktDTO = new FruktCreateDTO();
+        fruktDTO.setType(PEAR_NAME);
+        fruktDTO.setAmount(PEAR_AMOUNT);
+
+        fruktkorgDTO.setFruktList(Collections.singletonList(fruktDTO));
+
+        Fruktkorg persistedFruktkorg = new Fruktkorg();
+        persistedFruktkorg.setName(FRUKTKORG_NAME);
+        persistedFruktkorg.setId(1);
+        persistedFruktkorg.setLastChanged(NOW);
+
+        Frukt persistedFrukt = new Frukt();
+        persistedFrukt.setId(1);
+        persistedFrukt.setType(PEAR_NAME);
+        persistedFrukt.setAmount(PEAR_AMOUNT);
+        persistedFrukt.setFruktkorg(persistedFruktkorg);
+
+        persistedFruktkorg.setFruktList(Collections.singletonList(persistedFrukt));
+
+        when(fruktkorgService.saveFruktkorg(any())).thenReturn(persistedFruktkorg);
+
+        MvcResult result = mvc.perform(post("/v1/fruktkorg")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fruktkorgDTO)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Fruktkorg returnFruktKorg = objectMapper.readValue(result.getResponse().getContentAsString(),
+                Fruktkorg.class);
+
+        Assert.assertNotNull(returnFruktKorg);
+        Assert.assertEquals(persistedFruktkorg, returnFruktKorg);
+
+    }
+
+    @Test
+    public void updateFruktkorg() throws Exception {
+        FruktkorgUpdateDTO fruktkorgDTO = new FruktkorgUpdateDTO();
+        fruktkorgDTO.setId(1L);
+        fruktkorgDTO.setName(FRUKTKORG_NAME);
+        fruktkorgDTO.setLastChanged(NOW);
+
+        FruktUpdateDTO pearDTO = new FruktUpdateDTO();
+        pearDTO.setId(1l);
+        pearDTO.setType(PEAR_NAME);
+        pearDTO.setAmount(PEAR_AMOUNT);
+
+        fruktkorgDTO.setFruktList(Arrays.asList(pearDTO));
+
+        Fruktkorg persistedFruktkorg = new Fruktkorg();
+        persistedFruktkorg.setName(FRUKTKORG_NAME);
+        persistedFruktkorg.setId(1);
+        persistedFruktkorg.setLastChanged(NOW);
+
+        Frukt persistedFrukt = new Frukt();
+        persistedFrukt.setId(1);
+        persistedFrukt.setType(PEAR_NAME);
+        persistedFrukt.setAmount(PEAR_AMOUNT);
+        persistedFrukt.setFruktkorg(persistedFruktkorg);
+
+        persistedFruktkorg.setFruktList(Collections.singletonList(persistedFrukt));
+
+        when(fruktkorgService.saveFruktkorg(new Fruktkorg(fruktkorgDTO))).thenReturn(persistedFruktkorg);
+
+        MvcResult result = mvc.perform(put("/v1/fruktkorg")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fruktkorgDTO)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Fruktkorg returnFruktKorg = objectMapper.readValue(result.getResponse().getContentAsString(),
+                Fruktkorg.class);
+
+        Assert.assertNotNull(returnFruktKorg);
+        Assert.assertEquals(persistedFruktkorg, returnFruktKorg);
+
+    }
+
+    @Test
+    public void deleteFruktkorg() throws Exception {
+        mvc.perform(delete("/v1/fruktkorg/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteMissingFruktkorg() throws Exception {
+        doThrow(FruktkorgMissingException.class).when(fruktkorgService).deleteFruktkorg(1l);
+        mvc.perform(delete("/v1/fruktkorg/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(404));
+    }
+
     private List<Fruktkorg> createExampleFruktkorgar() {
-        int fruktkorgId = 0;
-        int fruktId = 0;
+        int fruktkorgId = 1;
+        int fruktId = 1;
         List<Fruktkorg> fruktkorgar = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Fruktkorg fruktkorg = new Fruktkorg();
